e74eb3323d6c32aa5c386ced40575fda9e57e3b2
Mohammad Hisham <mzoom26@hotmail.com>
2024-05-23 14:33:35 +0400

    App Strucure

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:mhk_portfolio_flutter/home.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyWebPortfolio());
